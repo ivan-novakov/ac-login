53123e98d5758cd1912eb0c2d27953f1927726c2
Ivan Novakov <ivan.novakov@debug.cz>
2014-05-29 07:27:03 +0200

    now using composer for deps

--- public/index.php
+++ public/index.php
@@ -21,7 +21,17 @@
  * 
  */
 
-require_once '../init.php';
+// require_once '../init.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+// The application root directory
+define('ACLOGIN_DIR', __DIR__ . '/../'); 
+
+// The directory with containing the configuration files
+define('ACLOGIN_CONFIG_DIR', ACLOGIN_DIR . 'config' . DIRECTORY_SEPARATOR);
+
+// The template directory
+define('ACLOGIN_TPL_DIR', ACLOGIN_DIR . 'tpl' . DIRECTORY_SEPARATOR);
 
 $app = new AcLogin_Application(array(
     'config_file' => ACLOGIN_CONFIG_DIR . 'aclogin.ini'
